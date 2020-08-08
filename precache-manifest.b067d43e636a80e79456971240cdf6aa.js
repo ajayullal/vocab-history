504a157f52886cc7e66c2d19ac4165de1ce09e12
Ajay Ullal <ajayullal91@gmail.com>
2020-08-08 19:06:59 +0530

    CP

---
+++ precache-manifest.b067d43e636a80e79456971240cdf6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449031641f44187245b703d7ea0c9849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c49ec87902b35e03ba8",
+    "url": "/static/css/main.11323bd9.chunk.css"
+  },
+  {
+    "revision": "375c157000c1fa003ff2",
+    "url": "/static/js/2.cb3582a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb3582a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c49ec87902b35e03ba8",
+    "url": "/static/js/main.7bdff964.chunk.js"
+  },
+  {
+    "revision": "f16dd915409b0386f0dd",
+    "url": "/static/js/runtime-main.0f0182d2.js"
+  }
+]);
